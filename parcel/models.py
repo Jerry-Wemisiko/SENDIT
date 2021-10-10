49910756144e4a4ed83f8f31fcee62a083e5234c
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-10-10 21:56:13 +0300

    update location models

--- parcel/models.py
+++ parcel/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-    
+    name = models.CharField(max_length=100, null=False)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
