42120c6f87b225b56236b8c62084326b24a504d4
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-10-10 22:03:55 +0300

    create order serializer for order model

--- parcel/serializers.py
+++ parcel/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['url', "first_name", "last_name", "phonenumber","username", 'email', "address", "orders"]
+
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
