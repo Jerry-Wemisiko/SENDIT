83f09405b6d192b9acb5bce5846bef9fa803cdf7
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-10-12 09:58:19 +0300

    new userview

--- parcel/views.py
+++ parcel/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import CustomUser, Order, Location
+from .serializers import UserSerializer, OrderSerializer, LocationSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = CustomUser.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
