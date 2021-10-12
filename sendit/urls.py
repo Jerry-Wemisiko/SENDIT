88145b37755d6438788c7f84c37de674584f00a4
kiptoo462 <victor.kiptoo@student.moringaschool.com>
2021-10-12 16:23:11 +0300

    Added the projet's urls

--- sendit/urls.py
+++ sendit/urls.py
@@ -1,21 +1,18 @@
-"""sendit URL Configuration
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+import parcel.views as parcel_views
+
+router = routers.DefaultRouter()
+router.register(r'users', parcel_views.UserViewSet)
+router.register(r'orders', parcel_views.OrderViewSet)
+router.register(r'locations', parcel_views.LocationViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/register/', parcel_views.UserCreateAPIView.as_view(), name='register'),
+    path('api/login/', parcel_views.UserLoginAPIView.as_view(), name='login'),
+    path('api/logout/', parcel_views.logout_view, name='logout'),
 ]
