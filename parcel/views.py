5ae3013025354fd63219a3d34431d8d94414760a
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-10-12 10:00:01 +0300

    new order apiendpoint

--- parcel/views.py
+++ parcel/views.py
@@ -11,3 +11,10 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = CustomUser.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Order.objects.all().order_by('-created_at')
+    serializer_class = OrderSerializer
